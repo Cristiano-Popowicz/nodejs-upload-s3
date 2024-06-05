7a76d22d20f99984f23acb4a19d2cfd4daa0f539
Cristiano Popowicz <crispopowicz@gmail.com>
2024-06-05 10:02:14 -0300

    added style to application

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+
 // Configuração da AWS
 AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
